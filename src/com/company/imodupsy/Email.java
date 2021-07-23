1f9244ceb218232862c3dc590acc6884e65620c0
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2021-07-23 12:29:11 +0100

    Add email and password

--- src/com/company/imodupsy/Email.java
+++ src/com/company/imodupsy/Email.java
@@ -8,7 +8,7 @@ public class Email {
     private String password;
     private String email;
     private String department;
-    private int mailBoxCapacity;
+    private int mailBoxCapacity = 500;
     private String alternateEmail;
     private String companySuffix = "fairy.com";
     private int defaultPasswordLength = 10;
@@ -17,20 +17,16 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
 
-        System.out.println("Email created for " + this.firstName + this.lastName );
-
         this.department = setDepartment();
-        System.out.println("Department " + this.department);
 
         this.password = randomPassword(defaultPasswordLength);
         System.out.println("Your password is  "  + this.password);
 
         this.email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + this.companySuffix;
-        System.out.println(this.email);
     }
 
     private String setDepartment() {
-        System.out.println("Department Codes\n1 for Sales\n2 for Development\n3 for Accounting\n0 for None\n Enter department code: ");
+        System.out.println("New worker: " + firstName  + " Department Codes:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for None\n Enter department code: ");
         Scanner keyboard = new Scanner(System.in);
         int deptChoice = keyboard.nextInt();
         if (deptChoice == 1) { return " Sales"; }
@@ -47,5 +43,35 @@ public class Email {
             password[i] = passwordSet.charAt(rand);
         }
         return new String(password);
+    }
+
+    public void setMailBoxCapacity(int capacity) {
+        this.mailBoxCapacity = capacity;
+    }
+
+    public void setAlternateEmail(String altEmail) {
+        this.alternateEmail = altEmail;
+    }
+
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    public int getMailBoxCapacity() {
+        return mailBoxCapacity;
+    }
+
+    public String getAlternateEmail() {
+        return alternateEmail;
+    }
+
+    public int getDefaultPasswordLength() {
+        return defaultPasswordLength;
+    }
+
+    public String showInfo() {
+        return "Display name: " + firstName + lastName +
+                "\nCompany email: " + email +
+                "\nMailbox capacity: " + mailBoxCapacity;
     }
 }
--- src/com/company/imodupsy/Main.java
+++ src/com/company/imodupsy/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	Email user1 = new Email("iModupsy", "Ade-Onojobi");
+        System.out.println(user1.showInfo());
     }
 }
